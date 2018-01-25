7b540aebf526226de4a34857fbdbb0d5e6cb1bfc
thomas <bayer@predic8.de>
2018-01-25 12:40:51 +0100

    solution

--- src/main/java/de/predic8/workshop/apigateway/ApiGatewayApplication.java
+++ src/main/java/de/predic8/workshop/apigateway/ApiGatewayApplication.java
@@ -1,23 +1,10 @@
 package de.predic8.workshop.apigateway;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.sleuth.metric.SpanMetricReporter;
-import org.springframework.cloud.sleuth.zipkin.HttpZipkinSpanReporter;
-import org.springframework.cloud.sleuth.zipkin.ZipkinProperties;
-import org.springframework.cloud.sleuth.zipkin.ZipkinRestTemplateCustomizer;
-import org.springframework.cloud.sleuth.zipkin.ZipkinSpanReporter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 
 @EnableDiscoveryClient
 @EnableZuulProxy
