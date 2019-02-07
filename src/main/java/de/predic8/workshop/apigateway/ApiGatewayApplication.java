f768b381656dcb9693d446a02f511ac532f324a9
Thomas Bayer <bayer@predic8.de>
2019-02-07 09:28:48 +0100

    Updated to newer versions

--- src/main/java/de/predic8/workshop/apigateway/ApiGatewayApplication.java
+++ src/main/java/de/predic8/workshop/apigateway/ApiGatewayApplication.java
@@ -3,13 +3,11 @@ package de.predic8.workshop.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableDiscoveryClient
 @EnableZuulProxy
 @SpringBootApplication
-@EnableHystrixDashboard
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
