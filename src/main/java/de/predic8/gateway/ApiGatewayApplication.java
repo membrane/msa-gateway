4f43791a49d6e0b77e0d49da9f6adaca3e287105
Thomas Bayer <bayer@predic8.de>
2019-02-23 18:55:42 +0100

    Zip dependency removed

--- src/main/java/de/predic8/gateway/ApiGatewayApplication.java
+++ src/main/java/de/predic8/gateway/ApiGatewayApplication.java
@@ -1,9 +1,11 @@
-package de.predic8.workshop.apigateway;
+package de.predic8.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+import static org.springframework.boot.SpringApplication.run;
 
 @EnableDiscoveryClient
 @EnableZuulProxy
@@ -11,7 +13,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
+		run(ApiGatewayApplication.class, args);
 	}
 
 }
--- src/test/java/de/predic8/gateway/ApiGatewayApplicationTests.java
+++ src/test/java/de/predic8/gateway/ApiGatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package de.predic8.workshop.apigateway;
+package de.predic8.gateway;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
